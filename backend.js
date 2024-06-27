1578d3e56d7afee86d896db525f983038630c258
unknown <imhimanshu369@gmail.com>
2024-06-28 02:25:44 +0530

    fixed backend

--- backend.js
+++ backend.js
@@ -7,6 +7,9 @@ const { Server } = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:3001", "https://khim-iam.github.io"];
+
+
 // const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 const io = new Server(server, {
   cors: {
@@ -22,12 +25,15 @@ const io = new Server(server, {
 const port = process.env.PORT || 3000; // Use environment variable or default to 3000
 
 
-app.use(cors()); // Use cors middleware
-// app.use(express.static('public'))
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html')
-// })
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 const instances = []
 let instanceIdCounter = 0
